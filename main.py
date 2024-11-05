66ff371a99d1675e44375170b012c8a7b8d2afb9
DantsevichIvan <iw.dantsevich@gmail.com>
2024-11-05 09:36:14 +0100

    Add app description, image, and links

--- main.py
+++ main.py
@@ -16,19 +16,20 @@ with col2:
     focusing on creating user-centered solutions that align with business needs."""
     st.info(content)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
-# for project in projects:
-#     value = project.split(";")
-#     st.image(f'images/{value[-1].strip()}', width=100)
-#     print("value", value)
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
