984ba1c0b67ae65a19aa0ca81301c1e272a6a963
DantsevichIvan <iw.dantsevich@gmail.com>
2024-11-06 09:39:30 +0100

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("Contact Us")
+
+with st.form(key='contact_form'):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+
+    if button:
+        print(button)
+        print(user_email)
+        print(message)
